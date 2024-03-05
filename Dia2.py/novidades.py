ba0d1d96cfe86e87d2db7a64951e948d294d5e82
Delieudo <delieudofilho@gmail.com>
2024-03-05 17:07:01 -0300

    atualizando

---
+++ Dia2.py/novidades.py
@@ -0,0 +1,16 @@
+# str.capitalize
+
+class carro:
+    def __init__(self, marca, modelo, ano ):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+def ligar (self):
+    print("seu veiculo ligou ", self.marca, self.ano, self.modelo)
+class SmartCar(carro):
+    def localizar(self):
+
+meu_carro = carro("chevrolet", "onix", 2020)
+meu_carro.ligar()
+
