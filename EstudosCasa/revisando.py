362fb93c969986965b378138836dbe92b08bd0cc
Delieudo <delieudofilho@gmail.com>
2024-03-05 22:01:50 -0300

    atulizaçao em casa

---
+++ EstudosCasa/revisando.py
@@ -0,0 +1,18 @@
+# faturamento = 1000
+# custo = 1800
+
+# lucro = faturamento - custo
+
+# if lucro >= 0:
+#     print("lucro ",lucro)
+# else:
+#     print("prejuizo!!!!!")
+#     print(lucro)
+
+
+
+produtos =["iphone","ipad","airpod"]
+novo_produto = input("digite aqui o produto:")
+
+if novo_produto in produtos:
+    print("produto cadastrado")
